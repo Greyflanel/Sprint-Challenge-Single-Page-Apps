d76edd1c2aa07f18dc596d4e3785b9e503a76bf6
Greyflanel <richardlovelace811@gmail.com>
2019-10-18 14:00:44 -0500

    Working on search feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
-import Header from "./components/Header.js";
+import SearchForm from "./components/SearchForm";
+import WelcomePage from "./components/WelcomePage";
+import CharacterList from "./components/CharacterList";
+import { Link, Route } from "react-router-dom";
 
 
 export default function App() {
   return (
+
     <main>
-      <Header />
+      
+<Link to="/search">Search</Link>
+<Link to="/"> Home </Link>
+<Link to="/characters"> Characters </Link>
+
+<Route exact path="/" component={WelcomePage} />
+<Route path="/characters" component={CharacterList}/>
+<Route path="/search" component={SearchForm} />
+
     </main>
   );
-}
+};
+
+
+
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,39 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+  useEffect(
+    () => {
+      axios
+        .get(`https://cors-anywhere.herokuapp.com/https://rick-api.herokuapp.com/api/character`)
+        .then(response => {
+          setCharacters(response.data.results);
+        })
+        .catch(error => console.log(error));
+    },
+    [characters, setCharacters]
+  );
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <CharCard>
+        {characters.map(char => (
+          <div key={char.name}>
+            {char.name}
+            <img src={char.image} alt={char.name} />
+          </div>
+        ))}
+      </CharCard>
     </section>
   );
 }
+
+const CharCard = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 5%;
+`;
